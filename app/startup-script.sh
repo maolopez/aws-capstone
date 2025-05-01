51234ad4769c9a8859064a6bca9a085bdefedb13
maolopez <maurizio.school@gmail.com>
2025-05-01 13:51:33 +0000

    adding startup script

--- app/startup-script.sh
+++ app/startup-script.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
 echo "Startup script"
+sudo yum install docker -y > /tmp/install-docker.txt
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+newgrp docker
+docker info
+git clone https://github.com/maolopez/ut_anagramma.git
+cd ut_anagramma/
+docker build -t ut-anagramma .
+docker run -t -i -p 8082:8082 ut-anagramma
